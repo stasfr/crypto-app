a510968f6028992f27333fcf4f5ab35483024a77
stas_fr <stas240600@yandex.ru>
2024-09-25 20:58:27 +0300

    supabase nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,8 @@ export default defineNuxtConfig({
     "@primevue/nuxt-module",
     "@nuxtjs/supabase",
   ],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+  },
 });
