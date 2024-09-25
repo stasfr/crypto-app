b526a9fc292cba16ce1f7fa4a564d0c7bd110219
stas_fr <stas240600@yandex.ru>
2024-09-25 20:24:52 +0300

    config, package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
+    "@nuxtjs/supabase",
+  ],
 });
